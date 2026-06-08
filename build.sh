2b16a1876466b27c5ddb943b9a9e3876332a85e4
xunicatt <contact.aniket.biswas@gmail.com>
2026-06-08 12:11:11 +0530

    added building unsigned ipa for ios

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+UNAME=$(uname)
+
 RAILM_API_URL="${RAILM_API_URL}"
 RAILM_API_TOKEN="${RAILM_API_TOKEN}"
 MAPBOX_API_TOKEN="${MAPBOX_API_TOKEN}"
@@ -15,6 +17,19 @@ build-railm() {
                       --dart-define=MAPBOX_TOKEN="${MAPBOX_API_TOKEN}"
     cd ..
     mv railm/build/app/outputs/apk/release/app-release.apk build/"railm-android-${RAILM_VERSION}.apk"
+
+    if [[ "$UNAME" == "Darwin" ]]; then
+        cd railm
+        flutter build ipa --release --no-codesign \
+                          --dart-define=URL="${RAILM_API_URL}" \
+                          --dart-define=TOKEN="${RAILM_API_TOKEN}" \
+                          --dart-define=MAPBOX_TOKEN="${MAPBOX_API_TOKEN}"
+        cd ..
+        cp -r railm/build/ios/archive/Runner.xcarchive/Products/Applications/Runner.app build/Payload
+        zip -r build/app.zip build/Payload
+        rm -r build/Payload
+        mv build/app.zip build/"railm-ios-${RAILM_VERSION}.ipa"
+    fi
 }
 
 build-railapi() {
