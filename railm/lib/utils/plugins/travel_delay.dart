f215fc09cc9b84d12b4f35f81f9487e66e9be46c
xunicatt <contact.aniket.biswas@gmail.com>
2026-06-15 15:18:26 +0530

    railm: added new plugin system
    Added a new plugin system which enables modularity and scalability for
    different status management and delay calculation. With the new design
    we can easily add more plugins from other sources to add more datas and
    predict delays more effectively.

---
+++ railm/lib/utils/plugins/travel_delay.dart
@@ -0,0 +1,39 @@
+import 'package:railm/components/map.dart';
+import 'package:railm/utils/plugin.dart';
+
+class TravelDelay extends Plugin {
+    MapData? data; 
+    num? value;
+
+    TravelDelay({
+        this.data
+    }) : super(
+        'Travel Delay',
+        'Get travel delay',
+    );
+
+    @override
+    Future<num> fetch() async {
+        if (value != null) {
+            return value!;
+        }
+
+        if (data == null) {
+            return 0;
+        }
+
+        final travelData = await MapView.fetchRoute(
+            'driving',
+            data!.lng1,
+            data!.lat1,
+            data!.lng2,
+            data!.lat2,
+        );
+        final routes = travelData['routes'];
+        final route = routes[data!.route];
+
+        value = (route['duration'] / 60).floor();
+
+        return value!;
+    }
+}
