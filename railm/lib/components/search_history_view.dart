ecca6d6716b73d991b03932e87168e0499897b68
xunicatt <contact.aniket.biswas@gmail.com>
2026-07-04 21:52:22 +0530

    railm: added LICENSE header

--- railm/lib/components/search_history_view.dart
+++ railm/lib/components/search_history_view.dart
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
+// Author: xunicatt
+// Project: railm (railm) 
+// Copyright (c) 2026 xunicatt <contact.aniket.biswas@gmail.com>
+
 import 'package:flutter/material.dart';
 import 'package:localstore/localstore.dart';
 import 'package:railm/models/search_history.dart';
